24817e3b102becff1705b61b507c7818f793bef9
Hit Snapper <Hit Snapper>
2015-11-11 21:43:03 +0100

    Fixed a moving suare

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import sys, pygame
-from square import *
-from images import *
+from Square import *
+import Images
 pygame.init()
 
+pygame.display.set_mode()
 size = width, height = 600, 400
 speed = [2, 2]
 black = 0, 0, 0
@@ -14,17 +15,34 @@ gameobjects = []
 
 coords = 10, 10
 
-bla = Square(coords, blue)
+bla = Square(coords, Images.blue)
 
 gameobjects.append(bla)
 
 
+
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT or \
+                (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            sys.exit()
+
+        #
+        # Checking keys
+        #
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            bla.coords = bla.coords[0], bla.coords[1] - 5
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            bla.coords = bla.coords[0] + 5, bla.coords[1]
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            bla.coords = bla.coords[0], bla.coords[1] + 5
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            bla.coords = bla.coords[0] - 5, bla.coords[1]
+
+    screen.fill(white)
 
     for obj in gameobjects:
         obj.draw(screen)
 
-    screen.fill(white)
+
     pygame.display.flip()
